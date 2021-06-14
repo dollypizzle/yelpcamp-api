dc0d421c971937ef8a6bb013b91790e5a43d606a
dollypizzle <dolapo.mabawonku@gmail.com>
2021-06-14 15:50:58 +0100

    cleaning and reviewing

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Campground = require('./campground');
+
 
 const userSchema = new mongoose.Schema(
   {
